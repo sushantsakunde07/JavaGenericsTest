4d4f59681dcee40a19edc0721eb715ae0a9c550b
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-05-23 05:25:41 +0530

    Added: Refactor all the three to the one generic class and find max.

--- src/main/java/com/bridgelabz/Maximum.java
+++ src/main/java/com/bridgelabz/Maximum.java
@@ -1,7 +1,19 @@
 package com.bridgelabz;
 
-public class Maximum {
-    public static <E extends Comparable> E getmaximum(E value1, E value2, E value3) {
+public class Maximum<E extends Comparable> {
+    E value1,value2,value3;
+
+    public Maximum(E value1, E value2, E value3) {
+        this.value1 = value1;
+        this.value2 = value2;
+        this.value3 = value3;
+    }
+    public E testMaximum(){
+        E testMax= testMaximum(value1,value2,value3);
+        return  testMax;
+    }
+
+    public static <E extends Comparable> E testMaximum(E value1, E value2, E value3) {
         E max = value1;
         if (value2.compareTo(value1) > 0 && value2.compareTo(value3) > 0)
             max = value2;
--- src/test/java/com/bridgelabz/MaximumTest.java
+++ src/test/java/com/bridgelabz/MaximumTest.java
@@ -1,60 +1,25 @@
 package com.bridgelabz;
 
+
 import org.junit.Test;
 import org.junit.Assert;
 
 public class MaximumTest {
     @Test
-    public void given3Values_WhenGetMaximum_ShouldReturnCorrectValue1() {
-        int finalResult = Maximum.getmaximum(9, 1, 4);
-        Assert.assertEquals(9, finalResult);
+    public void intMax() {
+        int actualValue = Maximum.testMaximum(9, 4, 1);
+        Assert.assertEquals(9, actualValue);
     }
 
     @Test
-    public void given3Values_WhenGetMaximum_ShouldReturnCorrectValue2() {
-        int finalResult = Maximum.getmaximum(1, 9, 4);
-        Assert.assertEquals(9, finalResult);
+    public void floatMax() {
+        float actualValue = Maximum.testMaximum(1.5f,9.1f,4.4f);
+        Assert.assertEquals(9.1f,actualValue,0);
     }
 
     @Test
-    public void given3Values_WhenGetMaximum_ShouldReturnCorrectValue3() {
-        int finalResult = Maximum.getmaximum(4, 1, 9);
-        Assert.assertEquals(9, finalResult);
-    }
-
-    @Test
-    public void given3FloatValues_WhenGetMaximum_ShouldReturnCorrectValue1() {
-        float finalResult = Maximum.getmaximum(9.6f, 1.4f, 4.4f);
-        Assert.assertEquals(9.6f, finalResult, 0.0);
-    }
-
-    @Test
-    public void given3FloatValues_WhenGetMaximum_ShouldReturnCorrectValue2() {
-        float finalResult = Maximum.getmaximum(1.4f, 9.6f, 4.4f);
-        Assert.assertEquals(9.6f, finalResult, 0.0);
-    }
-
-    @Test
-    public void given3FloatValues_WhenGetMaximum_ShouldReturnCorrectValue3() {
-        float finalResult = Maximum.getmaximum(4.4f, 1.4f, 9.6f);
-        Assert.assertEquals(9.6f, finalResult, 0.0);
-    }
-
-    @Test
-    public void given3StringValues_WhenGetMaximum_ShouldReturnCorrectValue1() {
-        String finalResult = Maximum.getmaximum("apple", "peach", "banana");
-        Assert.assertEquals("peach", finalResult);
-    }
-
-    @Test
-    public void given3StringValues_WhenGetMaximum_ShouldReturnCorrectValue2() {
-        String finalResult = Maximum.getmaximum("banana", "apple", "peach");
-        Assert.assertEquals("peach", finalResult);
-    }
-
-    @Test
-    public void given3StringValues_WhenGetMaximum_ShouldReturnCorrectValue3() {
-        String finalResult = Maximum.getmaximum("peach", "banana", "apple");
-        Assert.assertEquals("peach", finalResult);
+    public void stringMax() {
+        String actualValue = Maximum.testMaximum("apple","peach","banana");
+        Assert.assertEquals("peach",actualValue);
     }
 }
